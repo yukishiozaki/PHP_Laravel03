12465077079679fcc0d0fa01db4888093d7acfc4
yuki shiozaki <manten11@gmail.com>
2019-05-11 07:18:40 +0000

    first commit

---
+++ practice2.php
@@ -0,0 +1,7 @@
+<?php
+$total = 0;
+for($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+    
---
+++ practice3.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("lemon", "strawberry", "peach", "apple", "orange");
+foreach($fruits as $fruit){
+    echo "要素は" . "$fruit";
+    echo "\n";
+}
